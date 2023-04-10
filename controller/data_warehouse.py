de10ce72cf143c162e0be6a5c2dc18b0269dfb3a
kimhai <kimhaiareal@gmail.com>
2023-04-10 21:21:02 +0700

    commit

--- controller/data_warehouse.py
+++ controller/data_warehouse.py
@@ -35,9 +35,9 @@ class QueryDB:
     def get_total_order_orders_vegan_day_tbl_with(self, ngay_filter_list):
         ngay_filter_list = tuple(ngay_filter_list)
         if len(ngay_filter_list) == 1:
-            result = self.connection.execute(query.get_total_order_orders_vegan_day_tbl_with['single_day'] % (ngay_filter_list[0]))
+            result = self.connection.execute(text(query.get_total_order_orders_vegan_day_tbl_with['single_day'] % (ngay_filter_list[0])))
         else:
-            result = self.connection.execute(query.get_total_order_orders_vegan_day_tbl_with['multi_days'] % (ngay_filter_list,))
+            result = self.connection.execute(text(query.get_total_order_orders_vegan_day_tbl_with['multi_days'] % (ngay_filter_list,)))
         total_order_orders_vegan_day_tbl_with_df = pd.DataFrame(result.fetchall())
         return total_order_orders_vegan_day_tbl_with_df
 
